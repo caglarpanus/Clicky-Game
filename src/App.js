16079cf06a06eb211aa392fad494d227b2b182d9
caglarpanus <panuscaglar@gmail.com>
2018-04-20 10:02:15 -0400

    logic of game is being set

--- src/App.js
+++ src/App.js
@@ -8,6 +8,38 @@ import Title from "./components/Title";
 
 
 class App extends Component {
+
+  state = {
+    message:"Click an image to start playing!",
+    topScore:0,
+    currentScore:0,
+    images:images,
+    unSelectedImages:images
+  }
+
+  randomImage = image =>{
+    for(let i = image.length -1; i >0; --i) {
+      let x = Math.floor(Math.random() * (i + 1));
+    [image[i]], [image[x]] = [image[x]], [image[i]];
+    }
+  }
+
+  selectImage = image => {
+    const soccerPlayer = this.state.unSelectedImages.find(item => item.image === image);
+
+    if (soccerPlayer === undefined){
+      this.setState({
+        message:"Incorrect Guess!",
+        topScore:(this.state.currentScore > this.state.topScore) ? this.state.currentScore : this.state.topScore,
+        currentScore:0,
+        images:images,
+        unSelectedImages:images
+      })
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
